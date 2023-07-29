2472b04e48bd9b694bf07cbcec307836a38caba1
cdemukesh <cdemukesh@gmail.com>
2023-07-29 02:30:09 -0700

    Auto Push

--- vars/sample.groovy
+++ vars/sample.groovy
@@ -2,7 +2,7 @@
 
 def info(message) {
     echo "Hi, I am a function who's name is info"
-    echo "I am printing the value of message is $message"
+    echo "I am printing the value of message is ${message}"
 }
 
 // Calling the function info
