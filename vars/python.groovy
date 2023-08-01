40eea0a1bf1478c521f1aabf340250fac8520a96
cdemukesh <cdemukesh@gmail.com>
2023-08-01 10:59:04 -0700

    Auto Push

---
+++ vars/python.groovy
@@ -0,0 +1,34 @@
+def lintChecks() {
+    sh '''
+        echo Installing pylint for ${COMPONENT}
+        #pip3 install pylint
+        #pylint *.py
+        echo Lint Check Completed for ${COMPONENT}
+    '''
+}
+def call() {
+    pipeline {
+        agent { label 'WS' }
+        stages {
+            stage('Lint Checks') {                                          // Start of the stages
+                steps {
+                    script {
+                        lintChecks()
+                    }
+                }
+            }                                                                
+            stage('Code Compile') {
+                steps {
+                    sh "npm install"
+                }
+            }
+        }                                                                   // End of the stages
+    }
+}
+
+
+
+
+            //    script { 
+            //         sample.info('CATALOGUE')
+            //     }
