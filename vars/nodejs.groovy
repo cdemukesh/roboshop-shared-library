a5591f40ad1c79e21c074c78ecdb11a6af8d1d09
cdemukesh <cdemukesh@gmail.com>
2023-08-06 13:58:06 -0700

    Auto Push

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -11,6 +11,8 @@ def sonarChecks() {
     sh '''
         echo Sonar Checks In Progress
         sonar-scanner -Dsonar.projectKey=${COMPONENT}  -Dsonar.sources=. -Dsonar.login=c83e5f2df3ea9ac79a5029df5f37d7177535df6a -Dsonar.host.url=http://172.31.90.194:9000
+        curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate > sonar-quality-gate.sh
+        sonar-quality-gate.sh admin password 172.31.90.194 ${COMPONENT}
         echo Sonar Checks Completed
     '''
 }
@@ -37,6 +39,11 @@ def call() {
                     script {
                         sonarChecks()
                     }
+                }
+            }
+            stage('Testing') {
+                steps {
+                    sh "echo Testing In Progress"
                 }
             }
         }                                                                   // End of the stages
