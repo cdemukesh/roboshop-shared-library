437f35fdfa0738600dea2452d799228f2faf301d
cdemukesh <cdemukesh@gmail.com>
2023-08-06 19:37:08 -0700

    Auto Push

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -12,7 +12,7 @@ def sonarChecks() {
         echo Sonar Checks In Progress
         sonar-scanner -Dsonar.projectKey=${COMPONENT}  -Dsonar.sources=. -Dsonar.login=c83e5f2df3ea9ac79a5029df5f37d7177535df6a -Dsonar.host.url=http://172.31.90.194:9000
         curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate > sonar-quality-gate.sh
-        sonar-quality-gate.sh admin password 172.31.90.194 ${COMPONENT}
+        sonar-quality-gate.sh admin ${SONARCRED_USR} ${SONARCRED_PSW} ${SONARURL} ${COMPONENT}
         echo Sonar Checks Completed
     '''
 }
@@ -20,6 +20,10 @@ def sonarChecks() {
 def call() {
     pipeline {
         agent { label 'WS' }
+        environment {
+            SONARCRED = credentials("SONARCRED")
+            SONARURL = "172.31.90.194"
+        }
         stages {
             stage('Lint Checks') {                                          // Start of the stages
                 steps {
